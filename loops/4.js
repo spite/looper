018fb1972881e8ef87e2d810fdec0cf528bd034d
Jaume Sanchez <the.spite@gmail.com>
2018-03-07 23:19:43 +0000

    addded loop #4

---
+++ loops/4.js
@@ -0,0 +1,76 @@
+import {renderer, getCamera} from '/modules/three.js';
+const canvas = renderer.domElement;
+const camera = getCamera();
+const scene = new THREE.Scene();
+const group = new THREE.Group();
+
+const material = new THREE.MeshStandardMaterial({color: 0xb70000,metalness: 0, roughness: 1});
+const geometry = new THREE.BoxBufferGeometry(1,1,1);
+const cube = new THREE.Mesh(geometry, material);
+cube.castShadow = cube.receiveShadow = true;
+group.add(cube);
+const cube2 = new THREE.Mesh(geometry, material);
+cube2.castShadow = cube2.receiveShadow = true;
+group.add(cube2);
+const cube3 = new THREE.Mesh(geometry, material);
+cube3.castShadow = cube3.receiveShadow = true;
+group.add(cube3);
+scene.add(group);
+
+const directionalLight = new THREE.DirectionalLight( 0xffffff, .5 );
+directionalLight.position.set(-1,1,1);
+directionalLight.castShadow = true;
+scene.add( directionalLight );
+
+const directionalLight2 = new THREE.DirectionalLight( 0xffffff, .5 );
+directionalLight2.position.set(1,2,1);
+directionalLight2.castShadow = true;
+scene.add( directionalLight2 );
+
+const ambientLight = new THREE.AmbientLight(0x808080, .5);
+scene.add(ambientLight);
+
+const light = new THREE.HemisphereLight( 0xcefeff, 0xb3eaf0, .5 );
+scene.add( light );
+
+camera.position.set(4,4,4);
+camera.lookAt(cube.position);
+renderer.setClearColor(0xffffff,1);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+const loopDuration = 1;
+
+function InOutQuint(t) {
+  if ((t *= 2) < 1) return 0.5 * t * t * t * t * t;
+  return 0.5 * ((t -= 2) * t * t * t * t + 2);
+}
+
+function draw(startTime) {
+  const time = ( .001 * (performance.now()-startTime) ) % loopDuration;
+  const t = time * 2 * Math.PI / loopDuration;
+  const f = .75 + .25 * InOutQuint( .5 + .5 * Math.sin( t ));
+  const f2 = 1 + 2 * InOutQuint( .5 + .5 * Math.cos( t ));
+
+  //cube.rotation.x = time * Math.PI;
+  cube.scale.x = f;
+  cube.scale.y = f2;
+  cube.scale.z = f;
+
+  //cube2.rotation.x = time * Math.PI;
+  cube2.scale.x = f2;
+  cube2.scale.y = f;
+  cube2.scale.z = f;
+
+  //cube3.rotation.x = time * Math.PI;
+  cube3.scale.x = f;
+  cube3.scale.y = f;
+  cube3.scale.z = f2;
+
+  group.rotation.y = Math.PI / 2 * InOutQuint( time / loopDuration );
+ // group.rotation.x = 2 * Math.PI * Maf.smoothStep( 0, 1, time / loopDuration );
+
+  renderer.render(scene, camera);
+}
+
+export { draw, loopDuration, canvas };
