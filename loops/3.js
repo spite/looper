618ef6763cbb63979aa964f5e44e0c66d717f214
Jaume Sanchez Elias <jaumes@google.com>
2018-03-07 18:55:44 +0000

    added loop #3

---
+++ loops/3.js
@@ -0,0 +1,29 @@
+const loopDuration = 1;
+
+function draw(context) {
+  const canvas = context.canvas;
+  context.fillStyle = '#fff';
+  context.lineWidth = 1;
+  context.fillRect(0,0,canvas.width, canvas.height);
+  const time = ( .001 * performance.now() ) % loopDuration;
+  
+  const d = 32;
+  const w = canvas.width;
+  const h = canvas.height;
+    
+  context.fillStyle = '#000000';
+  for( var y = -.5*h; y < 2*h; y+=d ){
+    for( var x = -.5*w; x < 2*w; x+=d ){
+      context.beginPath();
+      const r = .5 * d;
+      const a = time * 2 * Math.PI / loopDuration + .01 * (x + y);
+      const px = x + r * Math.cos(a);
+      const py = y + r * Math.sin(a);
+      const ar = 10 + 5 * Math.sin(a);
+      context.arc(px,py,ar,0,2*Math.PI);
+      context.fill();
+    }
+  }
+}
+
+export { draw, loopDuration };
