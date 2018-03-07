7c45052da106a537b45dc6398bfd00754bf2092f
Jaume Sanchez <the.spite@gmail.com>
2018-03-07 23:19:37 +0000

    added module for threejs webglrenderer

---
+++ modules/three.js
@@ -0,0 +1,19 @@
+function getWebGLRenderer() {
+  const renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer.setSize(800, 800);
+  const canvas = renderer.domElement;
+  canvas.width = 800;
+  canvas.height = 800;
+  canvas.style.width = '400px';
+  canvas.style.height = '400px';
+  document.body.appendChild(canvas);
+  return renderer;
+}
+
+const renderer = getWebGLRenderer();
+
+function getCamera() {
+  return new THREE.PerspectiveCamera(35,1,.1,100);
+}
+
+export { renderer, getCamera };
