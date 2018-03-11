6e2ca25578c6b2fd3c0008e757dfdcc03455b816
Jaume Sanchez <the.spite@gmail.com>
2018-03-11 17:47:20 +0000

    added rounded extruded polygon

---
+++ modules/three-rounded-extruded-polygon.js
@@ -0,0 +1,26 @@
+function RoundedExtrudedPolygonGeometry(radius, length, sides, steps = 1, bevelThickness = .01, bevelSize = .01, bevelSteps = 1 ) {
+
+  var polygonShape = new THREE.Shape();
+  polygonShape.moveTo(radius, 0);
+  for (let j=0; j<sides; j++) {
+    const a = j * 2 * Math.PI / sides;
+    const x = radius * Math.cos(a);
+    const y = radius * Math.sin(a);
+    polygonShape.lineTo(x, y);
+  }
+
+  var extrudeSettings = {
+    steps: steps,
+    amount: length,
+    bevelEnabled: true,
+    bevelThickness: bevelThickness,
+    bevelSize: bevelSize,
+    bevelSegments: bevelSteps
+  };
+
+  const geometry = new THREE.ExtrudeBufferGeometry( polygonShape, extrudeSettings );
+  return geometry;
+
+}
+
+export default RoundedExtrudedPolygonGeometry;
