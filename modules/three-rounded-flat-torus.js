9c4d8f41aeff9978540ed8d9c1a75c8c69b872aa
Jaume Sanchez <the.spite@gmail.com>
2018-03-20 20:13:42 +0000

    added flat torus

---
+++ modules/three-rounded-flat-torus.js
@@ -0,0 +1,20 @@
+function RoundedFlatTorus(radius1, radius2, radius3 ) {
+
+  const cx = radius1;
+  const cy = 0;
+  const r = .1;
+  const pts = [];
+  for (let a=0; a<2*Math.PI+.2; a+=.1) {
+    const x = Math.pow(Math.abs(Math.cos(a)),.5)*r*Math.sign(Math.cos(a));
+    const y = Math.pow(Math.abs(Math.sin(a)),.5)*r*Math.sign(Math.sin(a));
+    pts.push(new THREE.Vector3(cx+radius2*x,cy+radius3*y,0));
+  }
+  const geometry = new THREE.LatheGeometry( pts, 72 );
+  geometry.computeVertexNormals();
+  geometry.computeFaceNormals();
+  geometry.normalsNeedUpdate = true;
+
+  return geometry;
+}
+
+export default RoundedFlatTorus;
