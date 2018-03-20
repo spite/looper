30d5dbd93980c91adf8f2e1653508dd4dd29a492
Jaume Sanchez <the.spite@gmail.com>
2018-03-20 20:13:51 +0000

    fixed easings

--- modules/easings.js
+++ modules/easings.js
@@ -130,7 +130,7 @@ const Easings = {
     return -Math.pow(2, -10 * t) + 1;
   },
   InOutExpo : function (t){
-    if (t==0) return b;
+    if (t==0) return 0;
     if (t==1) return 1;
     if ((t/=.5) < 1) return .5 * Math.pow(2, 10 * (t - 1));
     return .5 * (-Math.pow(2, -10 * --t) + 2);
