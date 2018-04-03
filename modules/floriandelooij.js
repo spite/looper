f60a1103cf555882bd8889d8e6707bb47a2fb5b1
Jaume Sanchez <the.spite@gmail.com>
2018-04-03 18:31:37 +0100

    added color module

---
+++ modules/floriandelooij.js
@@ -0,0 +1,28 @@
+const palette1 = {
+  base: [
+    '#2b2e2d',
+  ],
+  range: [
+    '#20a0aa',
+    '#ec4039',
+    '#ffae12',
+  ],
+};
+
+const palette2 = {
+  base: [
+    '#262626',
+  ],
+  range: [
+    '#612e8d',
+    '#c22286',
+    '#ea225e',
+    '#ed5b35',
+    '#f5b52e',
+    '#81c540',
+    '#00a396',
+    '#1674bc',
+  ],
+};
+
+export {palette1, palette2}
