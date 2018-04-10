96044fd3b701b9bd59bc6df1bf404251730ca2bb
Jaume Sanchez <the.spite@gmail.com>
2018-04-10 23:45:59 +0100

    added offset and correct calculations

--- modules/canvas-chromatic-aberration.js
+++ modules/canvas-chromatic-aberration.js
@@ -1,19 +1,19 @@
-function doChromaticAberration(canvas) {
+function doChromaticAberration(canvas, offset) {
   const context = canvas.getContext('2d');
   const imageData = context.getImageData(0,0,canvas.width,canvas.height);
   const targetImageData = context.createImageData(canvas.width,canvas.height);
   let ptr = 0;
-  const offset = 2;
   for (let y=0; y<canvas.height; y++) {
     for (let x=0; x<canvas.width; x++) {
       const dx = .5*canvas.width-x;
       const dy = .5*canvas.height-y;
       const d = Math.sqrt(dx*dx+dy*dy);
-      const tx1 = x + (1+offset)*dx/d;
-      const ty1 = y + (1+offset)*dy/d;
+      const dir = {x:dx/d, y:dy/d};
+      const tx1 = .5*canvas.width + offset*d*dir.x;
+      const ty1 = .5*canvas.height + offset*d*dir.y;
       const tPtr1 = (Math.round(ty1)*canvas.width + Math.round(tx1))*4;
-      const tx2 = x + (1-offset)*dx/d;
-      const ty2 = y + (1-offset)*dy/d;
+      const tx2 = .5*canvas.width - offset*d*dir.x;
+      const ty2 = .5*canvas.height - offset*d*dir.y;
       const tPtr2 = (Math.round(ty2)*canvas.width + Math.round(tx2))*4;
       targetImageData.data[ptr] = imageData.data[tPtr1];
       targetImageData.data[ptr+1] = imageData.data[ptr+1];
