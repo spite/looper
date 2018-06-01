9ca452e13c36065407a91ab8f6b81fd01c9ef5f5
Jaume Sanchez <the.spite@gmail.com>
2018-06-01 22:03:48 +0100

    cleanup

--- loops/84.js
+++ loops/84.js
@@ -43,11 +43,6 @@ function drawEllipse(x,y,a,b,theta,s,e) {
   context.restore();
 }
 
-function calcCircleRadius(x,y,r) {
-    var d = Math.sqrt(x*x+y*y);
-    return r - d;
-}
-
 function pointInEllipse(a,b,theta,t) {
   const x = a * Math.cos(t*Maf.TAU) * Math.cos(theta) - b * Math.sin(t*Maf.TAU) * Math.sin(theta);
   const y = a * Math.cos(t*Maf.TAU) * Math.sin(theta) + b * Math.sin(t*Maf.TAU) * Math.cos(theta);
