afd52122aac723a25ef28b38bb791c7245a3a820
Jaume Sanchez <the.spite@gmail.com>
2018-06-03 00:10:40 +0100

    added #86

---
+++ loops/86.js
@@ -0,0 +1,80 @@
+import THREE from '../third_party/three.js';
+import context from '../modules/context2d.js';
+import easings from '../modules/easings.js';
+import Maf from '../modules/maf.js';
+
+const canvas = context.canvas;
+
+const loopDuration = 2;
+
+const circles = [];
+for (let j=0; j<9; j++) {
+  circles.push({});
+}
+
+function drawLine(path,x1,y1,x2,y2,curvature) {
+  const cx = x1 + ( x2 - x1 ) / 2;
+  const cy = y1 + ( y2 - y1 ) / 2;
+  const n = new THREE.Vector2(cx,cy).normalize().multiplyScalar(curvature);
+  path.quadraticCurveTo(cx+n.x,cy+n.y,x2,y2);
+}
+
+function drawCircle(x,y,r,angle,f) {
+  const ax = 4;
+  context.save();
+  context.translate(-x,-y);
+
+  const path = new Path2D();
+  const x0 = r * Math.cos(angle);
+  const y0 = r * Math.sin(angle);
+  path.moveTo(x0,y0);
+  const sides = 5;
+  for (let j=0; j<sides; j++) {
+    const a = j * Maf.TAU / sides + angle;
+    const a2 = (j+1) * Maf.TAU / sides + angle;
+    const x = r * Math.cos(a);
+    const y = r * Math.sin(a);
+    const x2 = r * Math.cos(a2);
+    const y2 = r * Math.sin(a2);
+    drawLine(path,x,y,x2,y2,f);
+  }
+  path.closePath();
+
+  context.translate(-ax,0);
+  context.strokeStyle = '#ff0000';
+  context.stroke(path);
+  context.translate(ax,0);
+  context.strokeStyle = '#00ff00';
+  context.stroke(path);
+  context.translate(ax,0);
+  context.strokeStyle = '#0000ff';
+  context.stroke(path);
+
+  context.restore();
+}
+
+function draw(startTime) {
+  context.fillStyle = '#000';
+  context.fillRect(0,0,canvas.width, canvas.height);
+  const time = ( .001 * (performance.now()-startTime)) % loopDuration;
+  const t = time/loopDuration;
+
+  context.save();
+  context.translate(.5*canvas.width, .5*canvas.height);
+  const a = t*Maf.TAU/5;
+  context.strokeStyle = '#ffffff';
+  context.globalCompositeOperation = 'lighten';
+
+  context.lineWidth = 10;
+  circles.forEach( (c,i) => {
+    const tt = (t + i / circles.length ) % 1;
+    const r = i * 40;
+    context.globalAlpha = .4 + Maf.parabola(tt,1)*.4;
+    drawCircle(0,0,r,a,.39*r*Math.sin(tt*Maf.TAU));
+  });
+
+  context.restore();
+
+}
+
+export { draw, loopDuration, canvas };
