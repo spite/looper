cef4cd01dbbddc18ba6f80f7ce6b632b9fe23163
Jaume Sanchez <the.spite@gmail.com>
2018-07-03 18:46:47 +0100

    added #017

---
+++ loops/107.js
@@ -0,0 +1,148 @@
+import THREE from '../third_party/three.js';
+import {renderer, getCamera} from '../modules/three.js';
+import {Curves} from '../third_party/THREE.CurveExtras.js';
+import Maf from '../modules/maf.js';
+import easings from '../modules/easings.js';
+
+const canvas = renderer.domElement;
+const camera = getCamera();
+const scene = new THREE.Scene();
+const group = new THREE.Group();
+
+function getMaterial() {
+  const material = new THREE.MeshStandardMaterial({color: 0x5186a6, metalness: .1, roughness: .5});
+  material.onBeforeCompile = (shader) =>{
+    shader.vertexShader = shader.vertexShader.replace(
+      `varying vec3 vViewPosition;`,
+      `varying vec3 vViewPosition;
+  varying vec3 pos;
+  varying vec2 vUv;`);
+    shader.vertexShader = shader.vertexShader.replace(
+      `#include <defaultnormal_vertex>`,
+      `#include <defaultnormal_vertex>
+  pos = position;
+  vUv = uv;`);
+
+   shader.fragmentShader = shader.fragmentShader.replace(
+      `varying vec3 vViewPosition;`,
+      `varying vec3 vViewPosition;
+  varying vec3 pos;
+  varying vec2 vUv;
+
+  vec3 perturbNormalArb( vec3 surf_pos, vec3 surf_norm, vec2 dHdxy ) {
+  vec3 vSigmaX = dFdx( surf_pos );
+  vec3 vSigmaY = dFdy( surf_pos );
+  vec3 vN = surf_norm;    // normalized
+  vec3 R1 = cross( vSigmaY, vN );
+  vec3 R2 = cross( vN, vSigmaX );
+  float fDet = dot( vSigmaX, R1 );
+  vec3 vGrad = sign( fDet ) * ( dHdxy.x * R1 + dHdxy.y * R2 );
+  return normalize( abs( fDet ) * surf_norm - vGrad );
+}
+
+#define M_PI 3.1415926535897932384626433832795
+
+// form https://www.shadertoy.com/view/Xd2GR3
+vec4 hexagon( vec2 p ) {
+  vec2 q = vec2( p.x*2.0*0.5773503, p.y + p.x*0.5773503 );
+
+  vec2 pi = floor(q);
+  vec2 pf = fract(q);
+
+  float v = mod(pi.x + pi.y, 3.0);
+
+  float ca = step(1.0,v);
+  float cb = step(2.0,v);
+  vec2  ma = step(pf.xy,pf.yx);
+
+  float e = dot( ma, 1.0-pf.yx + ca*(pf.x+pf.y-1.0) + cb*(pf.yx-2.0*pf.xy) );
+
+  p = vec2( q.x + floor(0.5+p.y/1.5), 4.0*p.y/3.0 )*0.5 + 0.5;
+  float f = length( (fract(p) - 0.5)*vec2(1.0,0.85) );
+
+  return vec4( pi + ca - cb*ma, e, f );
+}
+
+`);
+
+   shader.fragmentShader = shader.fragmentShader.replace(
+      `vec4 diffuseColor = vec4( diffuse, opacity );`,
+      `vec4 diffuseColor = vec4( diffuse, opacity );
+  float r = sqrt(pos.x*pos.x+pos.y*pos.y+pos.z*pos.z);
+  float theta = acos(pos.z/r);
+  float phi = atan(pos.y,pos.x);
+
+  vec2 uv = (vUv+vec2(1.,opacity))*vec2(3.45,3.)*vec2(10.,5.) ;
+  vec4 h = hexagon(uv);
+  float strip = smoothstep(0.,.1,h.z);
+
+  vec4 h2 = hexagon(uv);
+  float stripOffset = smoothstep(.5,1.,h2.z);
+  float modifiedRoughness = strip;
+  diffuseColor.rgb = vec3(.5+.5*strip);`);
+
+    shader.fragmentShader = shader.fragmentShader.replace(
+      '#include <roughnessmap_fragment>',
+      `#include <roughnessmap_fragment>
+      roughnessFactor = modifiedRoughness;`
+    );
+
+    shader.fragmentShader = shader.fragmentShader.replace(
+      '#include <normal_fragment>',
+      `#include <normal_fragment>
+      normal = perturbNormalArb( -vViewPosition, normal, vec2( 0., stripOffset ) );`
+    );
+
+    shader.fragmentShader = `#extension GL_OES_standard_derivatives : enable
+    ${shader.fragmentShader}`;
+
+  }
+  return material;
+}
+
+const mesh = new THREE.Mesh(
+  new THREE.TorusBufferGeometry(3,1.5,36,200),
+  getMaterial()
+);
+mesh.receiveShadow = mesh.castShadow = true;
+group.add(mesh);
+
+scene.add(group);
+
+const directionalLight = new THREE.DirectionalLight( 0xffffff, .5 );
+directionalLight.position.set(-1,1,1);
+directionalLight.castShadow = true;
+scene.add( directionalLight );
+
+const directionalLight2 = new THREE.DirectionalLight( 0xffffff, .5 );
+directionalLight2.position.set(1,2,1);
+directionalLight2.castShadow = true;
+scene.add( directionalLight2 );
+
+const ambientLight = new THREE.AmbientLight(0x808080, .5);
+scene.add(ambientLight);
+
+const light = new THREE.HemisphereLight( 0xcefeff, 0xb3eaf0, .5 );
+scene.add( light );
+
+camera.position.set(10,-10,10);
+camera.lookAt(group.position);
+renderer.setClearColor(0xffffff,1);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+const loopDuration = 4;
+const cameraOffset = new THREE.Vector3();
+
+function draw(startTime) {
+
+  const time = ( .001 * (performance.now()-startTime)) % loopDuration;
+  const t = time / loopDuration;
+
+  mesh.material.opacity = t;
+  mesh.rotation.y  = Maf.TAU * t;
+
+  renderer.render(scene, camera);
+}
+
+export { draw, loopDuration, canvas };
