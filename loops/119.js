908b27b85c1c642fe7a287f61be63aaf98db0270
Jaume Sanchez <the.spite@gmail.com>
2018-07-10 18:49:14 +0100

    added #119

---
+++ loops/119.js
@@ -0,0 +1,73 @@
+import THREE from '../third_party/three.js';
+import {renderer, getCamera} from '../modules/three.js';
+import Maf from '../modules/maf.js';
+import easings from '../modules/easings.js';
+import RoundedExtrudedPolygonGeometry from '../modules/three-rounded-extruded-polygon.js';
+
+const canvas = renderer.domElement;
+const camera = getCamera();
+const scene = new THREE.Scene();
+const group = new THREE.Group();
+
+const whiteMaterial = new THREE.MeshStandardMaterial({color:0xffffff});
+const blackMaterial = new THREE.MeshStandardMaterial({color:0x000000});
+
+const MAX = 15;
+const SIDES = 72;
+const objects = [];
+for (let j=0; j<MAX; j++) {
+  const r = MAX-j;
+  const h = 1 + j;
+  const mesh = new THREE.Mesh(
+    new RoundedExtrudedPolygonGeometry(2*r,h,SIDES),
+    (j%2)?blackMaterial:whiteMaterial
+  );
+  objects.push(mesh);
+  mesh.receiveShadow = mesh.castShadow = true;
+  group.add(mesh);
+}
+group.scale.setScalar(.1);
+group.rotation.x =- Math.PI / 2;
+scene.add(group);
+
+const directionalLight = new THREE.DirectionalLight( 0xffffff, .5 );
+directionalLight.position.set(-1,1,1);
+directionalLight.castShadow = true;
+scene.add( directionalLight );
+
+const directionalLight2 = new THREE.DirectionalLight( 0xffffff, .5 );
+directionalLight2.position.set(1,2,1);
+directionalLight2.castShadow = true;
+scene.add( directionalLight2 );
+
+const ambientLight = new THREE.AmbientLight(0x808080, .5);
+scene.add(ambientLight);
+
+const light = new THREE.HemisphereLight( 0xcefeff, 0xb3eaf0, .5 );
+scene.add( light );
+
+camera.position.set(0,10,0);
+camera.lookAt(group.position);
+renderer.setClearColor(0xffffff,1);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+const loopDuration = 3;
+const cameraOffset = new THREE.Vector3();
+
+function draw(startTime) {
+
+  const time = ( .001 * (performance.now()-startTime)) % loopDuration;
+  const t = time / loopDuration;
+
+  objects.forEach( (o, id) => {
+    const offset = Maf.TAU * id / MAX;
+    o.position.x = .3 * id * Math.sin(t*Maf.TAU+offset);
+    o.position.y = .3 * id * Math.cos(t*Maf.TAU+offset);
+    o.rotation.z = t * Maf.TAU / SIDES + .1 * id;
+  });
+
+  renderer.render(scene, camera);
+}
+
+export { draw, loopDuration, canvas };
