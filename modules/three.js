2455436b887a3f81959d3607d412ec222529f056
Jaume Sanchez <the.spite@gmail.com>
2018-08-19 18:30:00 +0100

    reformatted

--- modules/three.js
+++ modules/three.js
@@ -1,7 +1,7 @@
 import THREE from '../third_party/three.js';
 
 function getWebGLRenderer() {
-  const renderer = new THREE.WebGLRenderer({antialias: true});
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(800, 800);
   const canvas = renderer.domElement;
   canvas.style.width = '400px';
@@ -12,11 +12,11 @@ function getWebGLRenderer() {
 const renderer = getWebGLRenderer();
 
 function getCamera(fov) {
-  return new THREE.PerspectiveCamera(fov?fov:35,1,.1,100);
+  return new THREE.PerspectiveCamera(fov ? fov : 35, 1, .1, 100);
 }
 
-function getOrthoCamera(w,h) {
-  return new THREE.OrthographicCamera(-w,w,h,-h,-100,100);
+function getOrthoCamera(w, h) {
+  return new THREE.OrthographicCamera(-w, w, h, -h, -100, 100);
 }
 
 export { renderer, getCamera, getOrthoCamera };
