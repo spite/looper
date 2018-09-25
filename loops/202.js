80de4f7df38d5e1ce41c7b476fc3605dc6a8839d
Jaume Sanchez <the.spite@gmail.com>
2018-09-25 22:00:11 +0100

    added #202

---
+++ loops/202.js
@@ -0,0 +1,152 @@
+import THREE from '../third_party/three.js';
+import { renderer, getOrthoCamera } from '../modules/three.js';
+import Maf from '../modules/maf.js';
+import { palette2 as palette } from '../modules/floriandelooij.js';
+import OrbitControls from '../third_party/THREE.OrbitControls.js';
+import { InstancedGeometry, getInstancedMeshStandardMaterial, getInstancedDepthMaterial } from '../modules/instanced.js';
+import { gradientLinear } from '../modules/gradient.js';
+import RoundedExtrudedPolygonGeometry from '../modules/three-rounded-extruded-polygon.js';
+import easings from '../modules/easings.js';
+import pointsOnSphere from '../modules/points-sphere.js';
+
+import Painted from '../modules/painted.js';
+
+const painted = Painted(renderer, { minLevel: -.1 });
+
+palette.range = ["#277016", "#D23D0E", "#53483F", "#A60B10", "#D23D0E", "#277016"];
+
+const canvas = renderer.domElement;
+const camera = getOrthoCamera(.75, .75);
+const controls = new OrbitControls(camera, canvas);
+const scene = new THREE.Scene();
+const group = new THREE.Group();
+
+const gradient = new gradientLinear(palette.range);
+
+const geometry = new THREE.CylinderBufferGeometry(.33, 0, 2, 18);
+const mRot = new THREE.Matrix4().makeRotationX(Math.PI / 2);
+geometry.applyMatrix(mRot);
+const material = getInstancedMeshStandardMaterial({ color: 0xffffff, metalness: .1, roughness: .8 }, { colors: true });
+const depthMaterial = getInstancedDepthMaterial();
+const instancedGeometry = new InstancedGeometry(geometry, { colors: true });
+const instancedMesh = new THREE.Mesh(instancedGeometry.geometry, material);
+instancedMesh.frustumCulled = false;
+instancedMesh.castShadow = true;
+instancedMesh.receiveShadow = true;
+instancedMesh.customDepthMaterial = depthMaterial;
+group.add(instancedMesh);
+
+const posValues = instancedGeometry.positions.values;
+const quatValues = instancedGeometry.quaternions.values;
+const scaleValues = instancedGeometry.scales.values;
+const colorValues = instancedGeometry.colors.values;
+
+const OBJECTS = 800;
+const points = pointsOnSphere(OBJECTS);
+
+let ptr = 0;
+for (let j = 0; j < OBJECTS; j++) {
+  posValues[ptr * 3] = 0;
+  posValues[ptr * 3 + 1] = 0;
+  posValues[ptr * 3 + 2] = 0;
+  scaleValues[ptr * 3] = 1;
+  scaleValues[ptr * 3 + 1] = 1;
+  scaleValues[ptr * 3 + 2] = 1;
+  quatValues[ptr * 4] = 0;
+  quatValues[ptr * 4 + 1] = 0;
+  quatValues[ptr * 4 + 2] = 0;
+  quatValues[ptr * 4 + 3] = 1;
+  colorValues[ptr * 4] = .5;
+  colorValues[ptr * 4 + 1] = .5;
+  colorValues[ptr * 4 + 2] = .5;
+  colorValues[ptr * 4 + 3] = 1;
+  ptr++;
+}
+
+instancedGeometry.update(OBJECTS);
+
+group.scale.setScalar(.1);
+
+scene.add(group);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, .5);
+directionalLight.position.set(-2, 2, 2);
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.near = -1;
+directionalLight.shadow.camera.far = 10;
+scene.add(directionalLight);
+
+const directionalLight2 = new THREE.DirectionalLight(0xffffff, .5);
+directionalLight2.position.set(1, 2, 1);
+directionalLight2.castShadow = true;
+directionalLight2.shadow.camera.near = -4;
+directionalLight2.shadow.camera.far = 10;
+scene.add(directionalLight2);
+
+const ambientLight = new THREE.AmbientLight(0x808080, .5);
+scene.add(ambientLight);
+
+const light = new THREE.HemisphereLight(palette.range[2], palette.range[1], .5);
+scene.add(light);
+
+camera.position.set(.2, .6, .8);
+camera.lookAt(new THREE.Vector3(0, 0, 0));
+renderer.setClearColor(palette.range[2], 1);
+scene.fog = new THREE.FogExp2(palette.range[2], .5);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+const loopDuration = 2;
+
+const q = new THREE.Quaternion();
+const m = new THREE.Matrix4();
+const up = new THREE.Vector3(0, 1, 0);
+
+function draw(startTime) {
+
+  const time = (.001 * (performance.now() - startTime)) % loopDuration;
+  const t = time / loopDuration;
+
+  for (let ptr = 0; ptr < OBJECTS; ptr++) {
+
+    const p = points[ptr];
+    const a = Math.atan2(p.x, p.z);
+    const b = Math.atan2(p.y, p.z);
+    const s = easings.InOutQuad(.6 + .4 * Math.sin(Maf.TAU * Maf.mod(ptr / OBJECTS + t, 1), 1));
+
+    posValues[ptr * 3 + 0] = (2 + 3 * s) * p.x;
+    posValues[ptr * 3 + 1] = (2 + 3 * s) * p.y;
+    posValues[ptr * 3 + 2] = (2 + 3 * s) * p.z;
+
+    m.lookAt(p, group.position, up);
+    mRot.makeRotationX(easings.InOutQuad(Maf.mod(t + ptr / OBJECTS, 1)) * Maf.TAU);
+    m.multiply(mRot);
+    q.setFromRotationMatrix(m);
+
+    quatValues[ptr * 4 + 0] = q.x;
+    quatValues[ptr * 4 + 1] = q.y;
+    quatValues[ptr * 4 + 2] = q.z;
+    quatValues[ptr * 4 + 3] = q.w;
+
+    const s2 = Maf.map(0, 1, .5, 1, s);
+    scaleValues[ptr * 3 + 0] = s2;
+    scaleValues[ptr * 3 + 1] = s2;
+    scaleValues[ptr * 3 + 2] = s2;
+
+    const c = gradient.getAt(Maf.mod(ptr / OBJECTS + t, 1));
+    colorValues[ptr * 4 + 0] = c.r;
+    colorValues[ptr * 4 + 1] = c.g;
+    colorValues[ptr * 4 + 2] = c.b;
+  }
+
+  instancedGeometry.positions.update(OBJECTS);
+  instancedGeometry.quaternions.update(OBJECTS);
+  instancedGeometry.scales.update(OBJECTS);
+  instancedGeometry.colors.update(OBJECTS);
+
+  //group.rotation.y = t * Maf.TAU;
+
+  painted.render(scene, camera);
+}
+
+export { draw, loopDuration, canvas };
