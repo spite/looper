d38775ff8d2f313ac64e6ff89fa5772454f6e9bc
Jaume Sanchez <the.spite@gmail.com>
2018-10-05 20:52:31 +0100

    added #214

---
+++ loops/214.js
@@ -0,0 +1,113 @@
+import THREE from '../third_party/three.js';
+import { renderer, getCamera } from '../modules/three.js';
+import { MeshLine, MeshLineMaterial } from '../modules/three-meshline.js';
+import Maf from '../modules/maf.js';
+import { palette2 as palette } from '../modules/floriandelooij.js';
+import { gradientLinear } from '../modules/gradient.js';
+import OrbitControls from '../third_party/THREE.OrbitControls.js';
+import { Curves } from '../third_party/THREE.CurveExtras.js';
+
+import Painted from '../modules/painted.js';
+
+const painted = Painted(renderer, { minLevel: -.2 });
+
+palette.range = ["#296888", "#C39B4B", "#A24218", "#FCFCFB", "#093588"];
+//palette.range = ["#444444", "#555555"];
+
+const gradient = new gradientLinear(palette.range);
+const curve = new THREE.Curves.GrannyKnot();
+
+const canvas = renderer.domElement;
+const camera = getCamera();
+const scene = new THREE.Scene();
+const group = new THREE.Group();
+const controls = new OrbitControls(camera, canvas);
+
+camera.position.set(12, 0, 12);
+camera.lookAt(group.position);
+renderer.setClearColor(0xF2E9D9, 1);
+
+const strokeTexture = new THREE.TextureLoader().load('./assets/stroke.png');
+const resolution = new THREE.Vector2(canvas.width, canvas.height);
+
+const POINTS = 50;
+const meshes = [];
+
+function prepareMesh(w, c) {
+
+  var geo = new Float32Array(POINTS * 3);
+  for (var j = 0; j < geo.length; j += 3) {
+    geo[j] = geo[j + 1] = geo[j + 2] = 0;
+  }
+
+  var g = new MeshLine();
+  g.setGeometry(geo, function(p) { return p; });
+
+  const material = new MeshLineMaterial({
+    useMap: true,
+    map: strokeTexture,
+    color: gradient.getAt(c),
+    opacity: .9,
+    resolution: resolution,
+    sizeAttenuation: true,
+    lineWidth: w,
+    near: camera.near,
+    far: camera.far,
+    depthTest: false,
+    transparent: true,
+  });
+
+  var mesh = new THREE.Mesh(g.geometry, material);
+  mesh.geo = geo;
+  mesh.g = g;
+
+  return mesh;
+}
+
+const spread = .5;
+for (let i = 0; i < 40; i++) {
+  const w = Maf.randomInRange(.8, 1.2);
+  const radius = .05 * Maf.randomInRange(4.5, 5.5);
+  const color = Maf.randomInRange(0, 1);
+  const offset = Maf.randomInRange(0, .125 * Maf.TAU);
+  const range = .125 * Maf.TAU;
+  const mesh = prepareMesh(w, color);
+  mesh.position.set(Maf.randomInRange(-spread, spread), Maf.randomInRange(-spread, spread), Maf.randomInRange(-spread, spread));
+  group.add(mesh);
+  meshes.push({
+    mesh,
+    radius,
+    offset,
+    range,
+  });
+
+}
+group.scale.setScalar(.5);
+scene.add(group);
+
+const loopDuration = 4;
+
+function draw(startTime) {
+
+  const time = (.001 * (performance.now() - startTime)) % loopDuration;
+  const t = time / loopDuration;
+
+  meshes.forEach((m) => {
+    const geo = m.mesh.geo;
+    const g = m.mesh.g;
+    const range = m.range;
+    const r = m.radius;
+    for (var j = 0; j < geo.length; j += 3) {
+      const t2 = Maf.TAU - (t * Maf.TAU + j * range / geo.length + m.offset);
+      const p = curve.getPoint(Maf.mod(t2 / Maf.TAU, 1));
+      geo[j] = r * p.x;
+      geo[j + 1] = r * p.y;
+      geo[j + 2] = r * p.z;
+    }
+    g.setGeometry(geo);
+  });
+
+  painted.render(scene, camera);
+}
+
+export { draw, loopDuration, canvas };
