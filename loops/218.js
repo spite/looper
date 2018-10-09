d53d0096be4d89ab1cb9b288c5aabbb5b82f104e
Jaume Sanchez <the.spite@gmail.com>
2018-10-09 19:22:04 +0100

    update

--- loops/218.js
+++ loops/218.js
@@ -10,7 +10,7 @@ import Painted from '../modules/painted.js';
 
 const painted = Painted(renderer, { minLevel: -.5 });
 
-palette.range = ["#FD7555", "#FE4F2E", "#040720", "#EB9786", "#E02211", "#3A0724", "#F9C163"];
+palette.range = ["#F76F11", "#FE981F", "#FDD635", "#BA404E", "#1A244F", "#100E24"]; //["#B94E38", "#E16336", "#FBC29A", "#E69347", "#3D2621", "#D07347"];
 
 const gradient = new gradientLinear(palette.range);
 
@@ -20,9 +20,9 @@ const scene = new THREE.Scene();
 const group = new THREE.Group();
 const controls = new OrbitControls(camera, canvas);
 
-camera.position.set(10, 10, -10);
-camera.lookAt(group.position);
-renderer.setClearColor(0xF2E9D9, 1);
+camera.position.set(-14.188801625046665, 3.5903940304216024, 2.3806730609761573);
+camera.rotation.set(-2.6296344032623524, -1.003297393734392, -2.961287722325734);
+renderer.setClearColor(0, 1);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
@@ -35,8 +35,10 @@ const lines = [];
 const LINES = 50;
 for (let i = 0; i < LINES; i++) {
 
+  const radius = 20 + 2 * i;
   const range = Maf.TAU * Maf.randomInRange(.025, .05);
-  const points = Math.round(range * 200);
+  const length = Maf.TAU * radius * range;
+  const points = Math.round(2 * length);
   var geo = new Float32Array(points * 3);
   for (var j = 0; j < geo.length; j += 3) {
     geo[j] = geo[j + 1] = geo[j + 2] = 0;
@@ -57,8 +59,8 @@ for (let i = 0; i < LINES; i++) {
     depthWrite: true,
     depthTest: true,
     transparent: true,
-    opacity: .95,
-    alphaTest: .5 * .95,
+    opacity: 1,
+    alphaTest: .5,
   });
 
   var mesh = new THREE.Mesh(g.geometry, material);
@@ -67,15 +69,14 @@ for (let i = 0; i < LINES; i++) {
   mesh.frustumCulled = false;
   pivot.add(mesh);
 
-  lines.push({ points, mesh, i, range, offset: .1 * Maf.randomInRange(0, Maf.TAU), frequency: .5 });
+  const speed = 1;
+  lines.push({ radius, speed, points, mesh, i, range, offset: .5 * Maf.randomInRange(0, Maf.TAU), frequency: .5 });
 }
 group.add(pivot);
 scene.add(group);
 group.scale.setScalar(.1);
 
 const loopDuration = 4;
-
-const target = new THREE.Vector3();
 
 function fbm(x, y, z) {
   let value = 0.;
@@ -91,9 +92,9 @@ function fbm(x, y, z) {
 }
 
 function f(x, y, z) {
-  const x2 = x;
+  const x2 = 10 + x;
   const y2 = y;
-  const z2 = z;
+  const z2 = 20 + z;
   return 50 * fbm(x2, y2, z2); //10 * perlin.perlin3(x, y, z) + 5 * perlin.perlin3(x2, y2, z2);
 }
 
@@ -105,26 +106,23 @@ function draw(startTime) {
   lines.forEach((l, id) => {
     const geo = l.mesh.geo;
     const g = l.mesh.g;
-    const r = 20 + 2 * id;
-    const s = .1 * l.frequency * Maf.map(0, lines.length, .075, .1, id);
+    const r = l.radius;
+    const s = .05 * l.frequency * Maf.map(0, lines.length, .075, .1, id);
     const points = l.points;
     for (let j = 0; j < points; j++) {
       const y = 1;
-      const a = l.range * j * Maf.TAU / points + t * Maf.TAU + l.offset;
+      const a = l.range * j * Maf.TAU / points + l.speed * t * Maf.TAU + l.offset;
       const x = r * Math.cos(a);
       const z = r * Math.sin(a);
-
       const v = f(s * x, s * y, s * z);
-      geo[j * 3 + 0] = x;
+      geo[j * 3 + 0] = x + v * Math.cos(a);
       geo[j * 3 + 1] = v;
-      geo[j * 3 + 2] = z;
+      geo[j * 3 + 2] = z + v * Math.sin(a);
     }
     g.setGeometry(geo);
   });
 
-  const a = t * Maf.TAU;
-  target.set(40 * Math.cos(a), 0, 40 * Math.sin(a));
-  //  camera.lookAt(target);
+  group.rotation.y = t * Maf.TAU;
 
   painted.render(scene, camera);
 }
