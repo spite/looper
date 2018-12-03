fec360ed29b6637e6f9cb7c527c87e9957e5de63
Jaume Sanchez <the.spite@gmail.com>
2018-12-03 20:55:01 +0000

    added #271

---
+++ loops/271.js
@@ -0,0 +1,134 @@
+import THREE from '../third_party/three.js';
+import { renderer, getOrthoCamera } from '../modules/three.js';
+import Maf from '../modules/maf.js';
+import OrbitControls from '../third_party/THREE.OrbitControls.js';
+import easings from '../modules/easings.js';
+import { Post } from '../modules/lines.js';
+
+const canvas = renderer.domElement;
+const camera = getOrthoCamera(2, 2);
+const controls = new OrbitControls(camera, canvas);
+controls.screenSpacePanning = true;
+const scene = new THREE.Scene();
+const group = new THREE.Group();
+
+const post = new Post(renderer);
+
+
+function CurvedLineCurve(from, to) {
+  THREE.Curve.call(this);
+  this.from = from;
+  this.to = to;
+}
+
+CurvedLineCurve.prototype = Object.create(THREE.Curve.prototype);
+CurvedLineCurve.prototype.constructor = CurvedLineCurve;
+
+CurvedLineCurve.prototype.getPoint = function(t) {
+  const p = this.from.clone().lerp(this.to, t);
+  const p2 = this.from.clone().lerp(this.to, t).normalize();
+  return p.lerp(p2, .2);
+};
+
+
+function addLine(from, to) {
+  const curve = new CurvedLineCurve(from, to);
+  const geometry = new THREE.TubeBufferGeometry(curve, 36, .01, 50, false);
+  const mesh = new THREE.Mesh(geometry, material);
+  group.add(mesh);
+  return mesh;
+}
+
+function createGroup() {
+  const g = new THREE.Group();
+  [...arguments].forEach((a) => {
+    g.add(a)
+  });
+  return g;
+}
+
+const material = new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.DoubleSide });
+const h = 1.;
+const g1 = createGroup(
+  addLine(new THREE.Vector3(-.5, h, -.5), new THREE.Vector3(.5, h, -.5)),
+  addLine(new THREE.Vector3(.5, h, -.5), new THREE.Vector3(.5, h, .5)),
+  addLine(new THREE.Vector3(.5, h, .5), new THREE.Vector3(-.5, h, .5)),
+  addLine(new THREE.Vector3(-.5, h, .5), new THREE.Vector3(-.5, h, -.5)),
+);
+group.add(g1);
+
+addLine(new THREE.Vector3(-.5, -h, -.5), new THREE.Vector3(.5, -h, -.5));
+addLine(new THREE.Vector3(.5, -h, -.5), new THREE.Vector3(.5, -h, .5));
+addLine(new THREE.Vector3(.5, -h, .5), new THREE.Vector3(-.5, -h, .5));
+addLine(new THREE.Vector3(-.5, -h, .5), new THREE.Vector3(-.5, -h, -.5));
+
+addLine(new THREE.Vector3(-h, -.5, .5), new THREE.Vector3(-h, -.5, -.5));
+addLine(new THREE.Vector3(-h, -.5, -.5), new THREE.Vector3(-h, .5, -.5));
+addLine(new THREE.Vector3(-h, .5, -.5), new THREE.Vector3(-h, .5, .5));
+addLine(new THREE.Vector3(-h, .5, .5), new THREE.Vector3(-h, -.5, .5));
+
+addLine(new THREE.Vector3(h, -.5, .5), new THREE.Vector3(h, -.5, -.5));
+addLine(new THREE.Vector3(h, -.5, -.5), new THREE.Vector3(h, .5, -.5));
+addLine(new THREE.Vector3(h, .5, -.5), new THREE.Vector3(h, .5, .5));
+addLine(new THREE.Vector3(h, .5, .5), new THREE.Vector3(h, -.5, .5));
+
+addLine(new THREE.Vector3(.5, -.5, h), new THREE.Vector3(-.5, -.5, h));
+addLine(new THREE.Vector3(-.5, -.5, h), new THREE.Vector3(-.5, .5, h));
+addLine(new THREE.Vector3(-.5, .5, h), new THREE.Vector3(.5, .5, h));
+addLine(new THREE.Vector3(.5, .5, h), new THREE.Vector3(.5, -.5, h));
+
+addLine(new THREE.Vector3(.5, -.5, -h), new THREE.Vector3(-.5, -.5, -h));
+addLine(new THREE.Vector3(-.5, -.5, -h), new THREE.Vector3(-.5, .5, -h));
+addLine(new THREE.Vector3(-.5, .5, -h), new THREE.Vector3(.5, .5, -h));
+addLine(new THREE.Vector3(.5, .5, -h), new THREE.Vector3(.5, -.5, -h));
+
+group.rotation.set(Maf.PI / 3.3, 0, Maf.PI / 4);
+window.group = group;
+scene.add(group);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, .5);
+directionalLight.position.set(-2, 2, 2);
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.near = -1;
+directionalLight.shadow.camera.far = 10;
+scene.add(directionalLight);
+
+const directionalLight2 = new THREE.DirectionalLight(0xffffff, .5);
+directionalLight2.position.set(1, 2, 1);
+directionalLight2.castShadow = true;
+directionalLight2.shadow.camera.near = -4;
+directionalLight2.shadow.camera.far = 10;
+scene.add(directionalLight2);
+
+const ambientLight = new THREE.AmbientLight(0x808080, .5);
+scene.add(ambientLight);
+
+const light = new THREE.HemisphereLight(0x776E88, 0xffffff, .5);
+scene.add(light);
+
+camera.position.set(0, 0, 5);
+camera.lookAt(new THREE.Vector3(0, 0, 0));
+renderer.setClearColor(0, 1);
+scene.fog = new THREE.FogExp2(0, .2);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+const loopDuration = 4;
+
+function draw(startTime) {
+
+  const time = (.001 * (performance.now() - startTime)) % loopDuration;
+  const t = time / loopDuration;
+
+  if (t < .5) {
+    const t2 = Maf.map(0, .5, 0, 1, t);
+    group.rotation.z = Maf.PI / 4 + easings.InOutQuint(t2) * Maf.PI / 2;
+  } else {
+    const t2 = Maf.map(0, .5, 0, 1, t - .5);
+    group.rotation.x = Maf.PI / 3.3 + easings.InOutQuint(t2) * Maf.PI;
+  }
+
+  post.render(scene, camera);
+}
+
+export { renderer, draw, loopDuration, canvas };
---
+++ modules/lines.js
@@ -0,0 +1,92 @@
+import THREE from '../third_party/three.js';
+
+import { getFBO } from './fbo.js';
+import ShaderPass from '../modules/shader-pass.js';
+
+import orthoVertexShader from '../shaders/ortho.js';
+import vignette from '../shaders/vignette.js';
+import fxaa from '../shaders/fxaa.js';
+import rgbShift from '../shaders/rgb-shift.js';
+import { gammaCorrect, levelRange, finalLevels } from '../../shaders/levels.js';
+
+const antialiasFragmentShader = `
+precision highp float;
+
+uniform vec2 resolution;
+
+uniform sampler2D inputTexture;
+uniform float minLevel;
+uniform float maxLevel;
+
+varying vec2 vUv;
+${fxaa}
+${gammaCorrect}
+${levelRange}
+${finalLevels}
+
+void main() {
+  vec4 color = fxaa(inputTexture, vUv );
+  color.rgb = finalLevels(color.rgb, vec3(0.) / 255., vec3(1.5), vec3(255.)/ 255.);
+  gl_FragColor = color;
+}
+`;
+
+const rgbFragmentShader = `
+precision highp float;
+
+uniform vec2 resolution;
+
+uniform sampler2D inputTexture;
+
+varying vec2 vUv;
+${rgbShift}
+
+void main() {
+  gl_FragColor = rgbShift(inputTexture, vUv,resolution/20.);
+}
+`;
+
+
+function Post(renderer, params = {}) {
+
+  const w = renderer.getSize().width;
+  const h = renderer.getSize().height;
+
+  const colorFBO = getFBO(w, h);
+
+  const antialiasShader = new THREE.RawShaderMaterial({
+    uniforms: {
+      resolution: { value: new THREE.Vector2(w, h) },
+      inputTexture: { value: colorFBO.texture },
+      minLevel: { value: 0 },
+      maxLevel: { value: .8 }
+    },
+    vertexShader: orthoVertexShader,
+    fragmentShader: antialiasFragmentShader,
+  });
+  const antialiasPass = new ShaderPass(renderer, antialiasShader, w, h, THREE.RGBAFormat, THREE.UnsignedByteType, THREE.LinearFilter, THREE.LinearFilter, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping);
+
+  const rgbShader = new THREE.RawShaderMaterial({
+    uniforms: {
+      resolution: { value: new THREE.Vector2(w, h) },
+      inputTexture: { value: antialiasPass.fbo.texture },
+      minLevel: { value: 0 },
+      maxLevel: { value: .8 }
+    },
+    vertexShader: orthoVertexShader,
+    fragmentShader: rgbFragmentShader,
+  });
+  const rgbPass = new ShaderPass(renderer, rgbShader, w, h, THREE.RGBAFormat, THREE.UnsignedByteType, THREE.LinearFilter, THREE.LinearFilter, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping);
+
+  function render(scene, camera) {
+    renderer.render(scene, camera, colorFBO);
+    antialiasPass.render();
+    rgbPass.render(true);
+  }
+
+  return {
+    render
+  }
+}
+
+export { Post };
