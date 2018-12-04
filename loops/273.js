3f37cc60f0d899aef61b2bbbf9afcc06c9cd6426
Jaume Sanchez <the.spite@gmail.com>
2018-12-04 20:19:58 +0000

    rework

--- loops/273.js
+++ loops/273.js
@@ -30,54 +30,39 @@ CurvedLineCurve.prototype.getPoint = function(t) {
 
 function addLine(from, to) {
   const curve = new CurvedLineCurve(from, to);
-  const geometry = new THREE.TubeBufferGeometry(curve, 36, .0025, 50, false);
+  const geometry = new THREE.TubeBufferGeometry(curve, 36, .0025, 5, false);
   const mesh = new THREE.Mesh(geometry, material);
   return mesh;
 }
 
 const material = new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.DoubleSide });
 
+const groups = [];
 const group1 = new THREE.Group();
-const geo = new THREE.IcosahedronBufferGeometry(1, 2);
+const geo = new THREE.IcosahedronBufferGeometry(1, 1);
 const pos = geo.attributes.position.array;
 for (let j = 0; j < pos.length; j += 9) {
   const p1 = new THREE.Vector3(pos[j + 0], pos[j + 1], pos[j + 2]);
   const p2 = new THREE.Vector3(pos[j + 3], pos[j + 4], pos[j + 5]);
   const p3 = new THREE.Vector3(pos[j + 6], pos[j + 7], pos[j + 8]);
   const center = new THREE.Vector3().add(p1).add(p2).add(p3).divideScalar(1 / 3);
-  const dir1 = p1.clone().sub(center).normalize().multiplyScalar(.5);
-  const dir2 = p2.clone().sub(center).normalize().multiplyScalar(.5);
-  const dir3 = p3.clone().sub(center).normalize().multiplyScalar(.5);
+  const dir1 = p1.clone().sub(center).normalize().multiplyScalar(.25);
+  const dir2 = p2.clone().sub(center).normalize().multiplyScalar(.25);
+  const dir3 = p3.clone().sub(center).normalize().multiplyScalar(.25);
   p1.sub(dir1);
   p2.sub(dir2);
   p3.sub(dir3);
-  group1.add(addLine(p1, p2));
-  group1.add(addLine(p2, p3));
-  group1.add(addLine(p3, p1));
+  const g = new THREE.Group();
+  g.add(addLine(p1, p2));
+  g.add(addLine(p2, p3));
+  g.add(addLine(p3, p1));
+  group.add(g);
+  groups.push({
+    g,
+    id: ~~Maf.randomInRange(0, 3)
+  });
 }
 
-const group2 = new THREE.Group();
-const geo2 = new THREE.IcosahedronBufferGeometry(1, 1);
-const pos2 = geo2.attributes.position.array;
-for (let j = 0; j < pos2.length; j += 9) {
-  const p1 = new THREE.Vector3(pos2[j + 0], pos2[j + 1], pos2[j + 2]);
-  const p2 = new THREE.Vector3(pos2[j + 3], pos2[j + 4], pos2[j + 5]);
-  const p3 = new THREE.Vector3(pos2[j + 6], pos2[j + 7], pos2[j + 8]);
-  const center = new THREE.Vector3().add(p1).add(p2).add(p3).divideScalar(1 / 3);
-  const dir1 = p1.clone().sub(center).normalize().multiplyScalar(.9);
-  const dir2 = p2.clone().sub(center).normalize().multiplyScalar(.9);
-  const dir3 = p3.clone().sub(center).normalize().multiplyScalar(.9);
-  p1.sub(dir1);
-  p2.sub(dir2);
-  p3.sub(dir3);
-  group2.add(addLine(p1, p2));
-  group2.add(addLine(p2, p3));
-  group2.add(addLine(p3, p1));
-}
-group2.scale.setScalar(.95);
-
-group.add(group1);
-group.add(group2);
 scene.add(group);
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, .5);
@@ -100,29 +85,25 @@ scene.add(ambientLight);
 const light = new THREE.HemisphereLight(0x776E88, 0xffffff, .5);
 scene.add(light);
 
-camera.position.set(0, 0, 5);
+camera.position.set(0, 5, 1);
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 renderer.setClearColor(0, 1);
 scene.fog = new THREE.FogExp2(0, .2);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-const loopDuration = 5;
+const loopDuration = 3;
 
 function draw(startTime) {
 
   const time = (.001 * (performance.now() - startTime)) % loopDuration;
   const t = time / loopDuration;
 
-  if (t < .5) {
-    const t2 = Maf.map(0, .5, 0, 1, t);
-    group1.rotation.y = easings.InOutQuad(t2) * Maf.PI;
-    group2.rotation.z = easings.InOutQuad(t2) * Maf.PI;
-  } else {
-    const t2 = Maf.map(0, .5, 0, 1, t - .5);
-    group1.rotation.z = easings.InOutQuad(t2) * Maf.PI;
-    group2.rotation.y = easings.InOutQuad(t2) * Maf.PI;
-  }
+  groups.forEach((g, i) => {
+    const tt = easings.InOutQuint(Maf.smoothStep(0, 1, Maf.mod(t + g.id * .1, 1)));
+    g.g.scale.setScalar(1 + .1 * Maf.parabola(tt, 1));
+  });
+  group.rotation.z = t * Maf.PI;
 
   post.render(scene, camera);
 }
