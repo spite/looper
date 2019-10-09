e41c416a1a6ed68adee0a856b143f87b5a7d0434
Jaume Sanchez <the.spite@gmail.com>
2019-10-09 18:55:47 +0100

    added #331 added options to painted renderpass

---
+++ loops/331.js
@@ -0,0 +1,123 @@
+import THREE from '../third_party/three.js';
+import { renderer, getCamera } from '../modules/three.js';
+import { MeshLine, MeshLineMaterial } from '../modules/three-meshline.js';
+import Maf from '../modules/maf.js';
+import { palette2 as palette } from '../modules/floriandelooij.js';
+import { gradientLinear } from '../modules/gradient.js';
+import easings from '../modules/easings.js';
+import OrbitControls from '../third_party/THREE.OrbitControls.js';
+import Painted from '../modules/painted.js';
+
+const painted = Painted(renderer, { minLevel: -.1, maxLevel: .8, lightenPass: 0 });
+
+palette.range = ["#151718", "#586363", "#4D372F", "#719094", "#C8D1D0", "#98A7A0", "#AC5C39", "#AB9656"];
+const gradient = new gradientLinear(palette.range);
+
+const canvas = renderer.domElement;
+const camera = getCamera();
+const scene = new THREE.Scene();
+const group = new THREE.Group();
+const controls = new OrbitControls(camera, canvas);
+controls.screenSpacePanning = true
+
+camera.position.set(35, 15, -35).multiplyScalar(.075);
+camera.lookAt(group.position);
+renderer.setClearColor(0x242424, 1);
+
+const strokeTexture = new THREE.TextureLoader().load('./assets/brush2.png');
+const resolution = new THREE.Vector2(canvas.width, canvas.height);
+
+const N = 4 * 240;
+
+const geo = new Float32Array(N * 3);
+const radius = 2;
+const lineWidth = 1;
+
+function prepareMesh(w, c) {
+
+  var g = new MeshLine();
+  g.setGeometry(geo, function(p) { return p; });
+
+  const material = new MeshLineMaterial({
+    map: strokeTexture,
+    useMap: true,
+    color: gradient.getAt(c),
+    resolution: resolution,
+    sizeAttenuation: true,
+    lineWidth: w,
+    near: camera.near,
+    far: camera.far,
+    repeat: new THREE.Vector2(5, 1),
+    alphaTest: .75 * .5,
+    depthWrite: true,
+    depthTest: true,
+    transparent: true,
+    opacity: 1,
+    dashArray: new THREE.Vector2(1, 1),
+    dashOffset: 0,
+  });
+
+  var mesh = new THREE.Mesh(g.geometry, material);
+  mesh.geo = geo;
+  mesh.g = g;
+
+  return mesh;
+}
+
+const LINES = 80;
+const meshes = [];
+for (let j = 0; j < LINES; j++) {
+  const mesh = prepareMesh(.125 * Maf.randomInRange(.1, 2), Maf.randomInRange(0, 1));
+  group.add(mesh);
+  const offset = Maf.randomInRange(0, 1);
+  const vertices = new Float32Array(N * 3);
+  const mat = new THREE.Matrix4();
+  const RSEGS = 2 * 80;
+  const r1 = 1 + .25 * j / LINES;
+  const r2 = 1 * j / LINES;
+  const offAngle = -j * .2 * Maf.TAU / LINES;
+  for (let i = 0; i < (N - 1); i++) {
+    const segment = (i / RSEGS);
+    const ringAngle = i * Maf.TAU / RSEGS;
+    const segAngle = segment * 1 * Maf.TAU / ((N - 1) / RSEGS);
+    const o = 1;
+    const p = new THREE.Vector3(r1 * Math.cos(segAngle), 0, r1 * Math.sin(segAngle));
+    const d = new THREE.Vector3(o * r2 * Math.cos(ringAngle), o * r2 * Math.sin(ringAngle), 0);
+    mat.makeRotationY(-segAngle);
+    d.applyMatrix4(mat);
+    vertices[i * 3] = p.x + d.x;
+    vertices[i * 3 + 1] = p.y + d.y;
+    vertices[i * 3 + 2] = p.z + d.z;
+  }
+  vertices[(N - 1) * 3] = vertices[0];
+  vertices[(N - 1) * 3 + 1] = vertices[1]
+  vertices[(N - 1) * 3 + 2] = vertices[2];
+  mesh.material.uniforms.dashArray.value.set(1, 4 * j / LINES);
+  mesh.g.setGeometry(vertices);
+  mesh.rotation.y = offAngle;
+  mesh.scale.setScalar(5);
+  mesh.material.uniforms.repeat.value.x = 1 + j * 10 / LINES;
+  let speed = Math.floor(Maf.randomInRange(1, 2));
+  if (Math.random() > .5) speed *= -1;
+  meshes.push({ mesh, offset, speed });
+}
+group.scale.setScalar(.09);
+scene.add(group);
+
+const loopDuration = 3.5;
+const r = 2;
+
+function draw(t) {
+
+  meshes.forEach((m) => {
+    const tt = Maf.mod(m.speed * t, 1);
+    m.mesh.material.uniforms.dashOffset.value = -1 * tt - m.offset;
+  });
+
+  group.rotation.y = t * Maf.TAU;
+  group.rotation.z = easings.InQuad(Maf.parabola(t, 1)) * Maf.TAU / 16;
+
+  painted.render(scene, camera);
+}
+
+export { draw, loopDuration, canvas, renderer, camera };
--- modules/painted.js
+++ modules/painted.js
@@ -2,7 +2,7 @@ import THREE from '../third_party/three.js';
 
 import orthoVertexShader from '../shaders/ortho.js';
 import vignette from '../shaders/vignette.js';
-import fxaa from '../shaders/fxaa.js';
+import fxaa from '../shaders/fxaa-pixel-shift.js';
 import grayscale from '../shaders/grayscale.js';
 import sobel from '../shaders/sobel.js';
 import overlay from '../shaders/overlay.js';
@@ -13,7 +13,8 @@ import { blur5 } from '../shaders/fast-separable-gaussian-blur.js';
 import highPass from '../shaders/high-pass.js';
 import ShaderPass from '../modules/shader-pass.js';
 
-const antialiasFragmentShader = `
+const antialiasFragmentShader =
+  `
 precision highp float;
 
 uniform vec2 resolution;
@@ -31,7 +32,8 @@ void main() {
 }
 `;
 
-const grayscaleFragmentShader = `
+const grayscaleFragmentShader =
+  `
 precision highp float;
 
 uniform vec2 resolution;
@@ -49,7 +51,8 @@ void main() {
 }
 `;
 
-const edgesFragmentShader = `
+const edgesFragmentShader =
+  `
 precision highp float;
 
 uniform vec2 resolution;
@@ -60,11 +63,12 @@ varying vec2 vUv;
 ${sobel}
 
 void main() {
-  gl_FragColor = vec4(sobel(inputTexture, vUv),1.);
+  gl_FragColor = vec4(sobel(inputTexture, vUv, resolution.x/800.),1.);
 }
 `;
 
-const fragmentShader = `
+const fragmentShader =
+  `
 precision highp float;
 
 uniform vec2 resolution;
@@ -74,6 +78,7 @@ uniform sampler2D edgesTexture;
 uniform sampler2D grayscaleTexture;
 uniform float vignetteBoost;
 uniform float vignetteReduction;
+uniform float lightenPass;
 
 varying vec2 vUv;
 ${vignette}
@@ -93,7 +98,9 @@ void main() {
 
   vec4 hp = highPass(inputTexture, vUv);
   color = softLight(color, hp);
-  color = lighten(color, grayEdges);
+  if(lightenPass==1.) {
+    color = lighten(color, grayEdges);
+  }
 
   color = softLight(color, vec4(vec3(vignette(vUv, vignetteBoost, vignetteReduction)),1.));
 
@@ -104,8 +111,8 @@ void main() {
 
 function Painted(renderer, params = {}) {
 
-  const w = renderer.getSize().width;
-  const h = renderer.getSize().height;
+  let w = 1;
+  let h = 1;
 
   const colorFBO = new THREE.WebGLRenderTarget(w, h, {
     wrapS: THREE.ClampToEdgeWrapping,
@@ -126,7 +133,8 @@ function Painted(renderer, params = {}) {
     vertexShader: orthoVertexShader,
     fragmentShader: antialiasFragmentShader,
   });
-  const antialiasPass = new ShaderPass(renderer, antialiasShader, w, h, THREE.RGBAFormat, THREE.UnsignedByteType, THREE.LinearFilter, THREE.LinearFilter, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping);
+  const antialiasPass = new ShaderPass(renderer, antialiasShader, w, h, THREE.RGBAFormat, THREE.UnsignedByteType, THREE
+    .LinearFilter, THREE.LinearFilter, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping);
 
   const edgesShader = new THREE.RawShaderMaterial({
     uniforms: {
@@ -136,7 +144,8 @@ function Painted(renderer, params = {}) {
     vertexShader: orthoVertexShader,
     fragmentShader: edgesFragmentShader,
   });
-  const edgesPass = new ShaderPass(renderer, edgesShader, w, h, THREE.RGBAFormat, THREE.UnsignedByteType, THREE.LinearFilter, THREE.LinearFilter, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping);
+  const edgesPass = new ShaderPass(renderer, edgesShader, w, h, THREE.RGBAFormat, THREE.UnsignedByteType, THREE.LinearFilter,
+    THREE.LinearFilter, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping);
 
   const blurShader = new THREE.RawShaderMaterial({
     uniforms: {
@@ -147,8 +156,10 @@ function Painted(renderer, params = {}) {
     vertexShader: orthoVertexShader,
     fragmentShader: grayscaleFragmentShader,
   });
-  const blurHPass = new ShaderPass(renderer, blurShader, w, h, THREE.RGBAFormat, THREE.UnsignedByteType, THREE.LinearFilter, THREE.LinearFilter, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping);
-  const blurVPass = new ShaderPass(renderer, blurShader, w, h, THREE.RGBAFormat, THREE.UnsignedByteType, THREE.LinearFilter, THREE.LinearFilter, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping);
+  const blurHPass = new ShaderPass(renderer, blurShader, w, h, THREE.RGBAFormat, THREE.UnsignedByteType, THREE.LinearFilter,
+    THREE.LinearFilter, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping);
+  const blurVPass = new ShaderPass(renderer, blurShader, w, h, THREE.RGBAFormat, THREE.UnsignedByteType, THREE.LinearFilter,
+    THREE.LinearFilter, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping);
 
   const shader = new THREE.RawShaderMaterial({
     uniforms: {
@@ -157,33 +168,56 @@ function Painted(renderer, params = {}) {
       vignetteReduction: { value: .5 },
       inputTexture: { value: antialiasPass.fbo.texture },
       edgesTexture: { value: edgesPass.fbo.texture },
-      grayscaleTexture: { value: blurVPass.fbo.texture }
+      grayscaleTexture: { value: blurVPass.fbo.texture },
+      lightenPass: { value: params.lightenPass !== undefined ? params.lightenPass : 1 }
     },
     vertexShader: orthoVertexShader,
     fragmentShader: fragmentShader,
   });
-  const pass = new ShaderPass(renderer, shader, w, h, THREE.RGBAFormat, THREE.UnsignedByteType, THREE.LinearFilter, THREE.LinearFilter, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping);
+  const pass = new ShaderPass(renderer, shader, w, h, THREE.RGBAFormat, THREE.UnsignedByteType, THREE.LinearFilter,
+    THREE.LinearFilter, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping);
 
   function render(scene, camera) {
+
+    const size = renderer.getSize();
+    if (size.width !== w || size.height !== h) {
+      console.log(`Resize ${size.width}, ${size.height}`);
+      w = size.width;
+      h = size.height;
+      colorFBO.setSize(w, h);
+      antialiasPass.setSize(w, h);
+      antialiasShader.uniforms.resolution.value.set(w, h);
+      edgesPass.setSize(w, h);
+      edgesShader.uniforms.resolution.value.set(w, h);
+      blurHPass.setSize(w, h);
+      blurShader.uniforms.resolution.value.set(w, h);
+      blurVPass.setSize(w, h);
+      pass.setSize(w, h);
+      shader.uniforms.resolution.value.set(w, h);
+    }
+
     renderer.render(scene, camera, colorFBO);
     antialiasPass.shader.uniforms.inputTexture.value = colorFBO.texture;
     antialiasPass.shader.uniforms.minLevel.value = params.minLevel || .2;
+    antialiasPass.shader.uniforms.maxLevel.value = params.maxLevel || 1;
     antialiasPass.render();
     edgesPass.render();
+    const d = 2 * w / 800;
     blurHPass.shader.uniforms.inputTexture.value = edgesPass.fbo.texture;
-    blurHPass.shader.uniforms.direction.value.set(2, 0);
+    blurHPass.shader.uniforms.direction.value.set(d, 0);
     blurHPass.render();
     blurVPass.shader.uniforms.inputTexture.value = blurHPass.fbo.texture;
-    blurVPass.shader.uniforms.direction.value.set(0, 2);
+    blurVPass.shader.uniforms.direction.value.set(0, d);
     blurVPass.render();
     blurHPass.shader.uniforms.inputTexture.value = blurVPass.fbo.texture;
-    blurHPass.shader.uniforms.direction.value.set(2, 0);
+    blurHPass.shader.uniforms.direction.value.set(d, 0);
     blurHPass.render();
     blurVPass.shader.uniforms.inputTexture.value = blurHPass.fbo.texture;
-    blurVPass.shader.uniforms.direction.value.set(0, 2);
+    blurVPass.shader.uniforms.direction.value.set(0, d);
     blurVPass.render();
     pass.render();
     antialiasPass.shader.uniforms.minLevel.value = 0;
+    antialiasPass.shader.uniforms.maxLevel.value = 1;
     antialiasPass.shader.uniforms.inputTexture.value = pass.fbo.texture;
     antialiasPass.render(true);
   }
