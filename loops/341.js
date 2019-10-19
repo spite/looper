8eb0245218efd593920fec910523fd4cbc3ff5da
Jaume Sanchez <the.spite@gmail.com>
2019-10-19 16:54:47 +0100

    fixed time

--- loops/341.js
+++ loops/341.js
@@ -113,10 +113,10 @@ scene.add(group);
 const loopDuration = 5;
 const r = 2;
 
-function draw(t) {
+function draw(startTime) {
 
-  //const time = (.001 * (performance.now() - startTime)) % loopDuration;
-  //const t = time / loopDuration;
+  const time = (.001 * (performance.now() - startTime)) % loopDuration;
+  const t = time / loopDuration;
 
   meshes.forEach((m) => {
     const tt = Maf.mod(m.speed * t, 1);
